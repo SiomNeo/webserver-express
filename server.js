46906d0776bc6812face6fa30ccabe3319d98548
Sergio <sordonez19@gmail.com>
2020-05-31 14:19:58 -0600

    Primer commit

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+require('./hbs/helpers');
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'))
+
+//Express HBS
+hbs.registerPartials(__dirname + '/views/parciales', (err) => {});
+app.set('view engine', 'hbs');
+
+
+
+app.get('/', (req, res) => {
+    res.render('home', {
+        nombre: 'Sergio ORdoñez'
+    });
+
+})
+
+app.get('/about', (req, res) => {
+    res.render('about');
+
+})
+
+app.get('/data', (req, res) => {
+    let salida = {
+        nombre: 'Sergio',
+        edad: 39,
+        url: req.url
+    }
+    res.send(salida);
+
+})
+
+app.listen(port, () => {
+    console.log(`Escuchando peticiones en el puerto ${port}`);
+})
